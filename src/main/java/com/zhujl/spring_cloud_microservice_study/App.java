7ae4da755fdac2538e4f7d399746c7485a37415e
zjltq <zhujilong110@163.com>
2016-09-14 16:39:46 +0800

    Signed-off-by: zjltq <zhujilong110@163.com>

---
+++ src/main/java/com/zhujl/spring_cloud_microservice_study/App.java
@@ -0,0 +1,13 @@
+package com.zhujl.spring_cloud_microservice_study;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
